f455e59f01f57230dc381b3ddd4d34d7173ac5b7
Omar Belfeki <164212057+OmarBelfeki@users.noreply.github.com>
2025-02-03 21:37:07 +0100

    Create __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,11 @@
+from app.services.attendance_service import AttendanceService
+from app.services.exam_service import ExamService
+from app.services.fee_service import FeeService
+from app.services.notification_service import NotificationService
+
+__all__ = [
+    "AttendanceService",
+    ExamService,
+    FeeService,
+    NotificationService
+]
